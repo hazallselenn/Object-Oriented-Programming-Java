2e55afb8f0093216e1de84a31b2ab05f0f8d924a
Hazal <104279018+hazallselenn@users.noreply.github.com>
2022-10-26 04:23:22 +0300

    Add files via upload

---
+++ 2-week/denklem_1.java
@@ -0,0 +1,10 @@
+public class denklem_1 {
+
+   public static void main(String[] args) {
+
+    float sonuc,y=14,x=1.2f;
+    sonuc=((3*x)+(7/y))*((3*x)+(7/y));
+    System.out.println(sonuc);
+        
+    }
+}
---
+++ 2-week/denklem_2.java
@@ -0,0 +1,8 @@
+public class denklem_2 {
+
+    public static void main(String[] args) {
+        double sonuc,x=6,y=4;
+        sonuc=((x*x)+(2*y))/((y*y)/2);
+        System.out.println(sonuc);
+    }
+}
