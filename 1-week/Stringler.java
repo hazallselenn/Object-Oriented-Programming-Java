7d6808c3a17e3b34d90056baacab533954acdd64
Hazal <104279018+hazallselenn@users.noreply.github.com>
2022-10-26 04:20:50 +0300

    Add files via upload

---
+++ 1-week/Stringler.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.stringler;
+
+/**
+ *
+ * @author dell
+ */
+import java.util.*;
+public class Stringler {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       
+       String ad,soyad;
+       System.out.print("Ad:");
+       ad=sc.next();
+       System.out.print("Soyad:");
+       soyad=sc.next();
+       System.out.println(ad);
+       System.out.println(soyad);
+
+
+    }
+}
